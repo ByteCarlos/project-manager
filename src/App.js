d95ef3cf4fe0f775e2846a6a5c4590880ff6803c
Carlos Henrique Santos Silveira <109424074+ByteCarlos@users.noreply.github.com>
2023-11-12 16:27:50 -0300

    css modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
+
+  const nome = 'Maria'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testando CSS</h1>
+      <Frase />
+      <Frase />
+      <SayMyName nome="Carlos"/>
+      <SayMyName nome="João"/>
+      <SayMyName nome={nome}/>
+      <Pessoa 
+        nome="Rodrigo" 
+        idade="28" 
+        profissao="Programador" 
+        foto="https://via.placeholder.com/150"
+      />
     </div>
   );
 }
